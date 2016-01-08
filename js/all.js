6f9d556ab02361e10f7b6a651cc6b2ea14964ebd
yuankui <kui.yuan@fraudmetrix.cn>
2016-01-08 15:43:43 +0800

    add function

---
+++ js/all.js
@@ -0,0 +1,4 @@
+$nav = $('.header-nav.left');
+$link = $('<li class="header-nav-item"><a href="/explore" class="js-selected-navigation-item header-nav-link" data-hotkey="g p">Explore</a></li>');
+$link.appendTo($nav);
+
